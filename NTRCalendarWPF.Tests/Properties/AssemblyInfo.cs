c49f3d20cc83d1f3aa2cb9fcefe357ee18d34d29
Wojciech Gruszka <wojciech@gruszka.eu>
2017-11-02 00:21:58 +0100

    Testing base

---
+++ NTRCalendarWPF.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NTRCalendarWPF.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NTRCalendarWPF.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cc5ebbf9-1b34-4170-9096-f73d628ff559")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ NTRCalendarWPF.Tests/Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+using Rhino.Mocks;
+using NTRCalendarWPF.Model;
+
+namespace NTRCalendarWPF.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void RepoShouldAddEvents() {
+            var mockRepository = MockRepository.GenerateMock<FileCalendarEventRepository>();
+            var newEvent = new CalendarEvent();
+            mockRepository.AddEvent(newEvent);
+        }
+    }
+}
