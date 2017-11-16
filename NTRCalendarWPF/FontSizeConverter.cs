facc5b84b8938004dc6be1eabcf25cffe6cb77e4
Wojciech Gruszka <wojciech@pearvoid.pl>
2017-11-16 12:36:09 +0100

    some random shit

--- NTRCalendarWPF/FontSizeConverter.cs
+++ NTRCalendarWPF/FontSizeConverter.cs
@@ -3,14 +3,24 @@ using System.Globalization;
 using System.Windows.Data;
 
 namespace NTRCalendarWPF {
-    public class FontSizeConverter : IValueConverter {
+    public class FontSizeConverter : IMultiValueConverter {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var w = (double)values[0];
+            var h = (double)values[1];
+
+            return (w * h) / double.Parse((string)parameter);
+        }
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
             var size = (double?) value;
             return size / 10;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
             throw new NotImplementedException();
         }
+
     }
 }
