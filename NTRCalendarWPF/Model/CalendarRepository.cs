cb5c13f435778f0766ebdf41f968e7cc1f9ba537
Wojciech Gruszka <wojciech@gruszka.eu>
2017-11-26 16:06:30 +0100

    Repo upgrade

--- NTRCalendarWPF/Model/CalendarRepository.cs
+++ NTRCalendarWPF/Model/CalendarRepository.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Dynamic;
 using System.Linq;
 using System.Security.RightsManagement;
 
 namespace NTRCalendarWPF.Model {
     public class CalendarRepository {
+        public delegate void CalendarModifiedDelegate();
+
+        public event CalendarModifiedDelegate OnDataChanged;
+
         public List<Person> GetPeople() {
             using (var db = new StorageContext())
                 return db.People.ToList();
@@ -40,15 +45,42 @@ namespace NTRCalendarWPF.Model {
             }
         }
 
-        public void AddAppointment(string userID) {
-            var appointment = new Appointment { };
-            var attendance = new Attendance { };
+        public void AddAppointment(string userID, string title, DateTime start, DateTime end) {
+            var appointment = new Appointment {
+                Title = title,
+                StartTime = start,
+                EndTime = end,
+                AppointmentId = Guid.NewGuid()
+            };
+            var attendance = new Attendance {
+                Appointment = appointment,
+            };
+
             using (var db = new StorageContext()) {
-                db.People.First(a => a.UserID.Equals(userID)).Attendances.Add(attendance);
+                var person = db.People.First(a => a.UserID.Equals(userID));
+                attendance.Person = person;
+
                 db.Appointments.Add(appointment);
                 db.Attendances.Add(attendance);
                 db.SaveChanges();
             }
+            OnDataChanged?.Invoke();
+        }
+
+        public void RemoveAppointment(Appointment appointment) {
+            using (var db = new StorageContext()) {
+                db.Appointments.Remove(appointment);
+                db.SaveChanges();
+            }
+            OnDataChanged?.Invoke();
+        }
+
+        public void UpdateAppointment(Appointment appointment) {
+            using (var db = new StorageContext()) {
+                db.Appointments.AddOrUpdate(appointment);
+                db.SaveChanges();
+            }
+            OnDataChanged?.Invoke();
         }
     }
 }
